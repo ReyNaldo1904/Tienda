e02bbc847cbcc0309ca033537abe76fa72699cb4
ReyNaldo1904 <114126394+ReyNaldo1904@users.noreply.github.com>
2023-10-13 13:51:37 -0600

    Semana 5 Categorias

---
+++ src/main/java/com/tienda_v1/service/CategoriaService.java
@@ -0,0 +1,9 @@
+package com.tienda_v1.service;
+
+import com.tienda_v1.domain.Categoria;
+import java.util.List;
+
+
+public interface CategoriaService {
+    public List<Categoria> getCategorias(boolean activo);   
+}
