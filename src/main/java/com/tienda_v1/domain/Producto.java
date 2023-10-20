9e6078d5c62cea01dd0f8fa19204ed5ddd0dc0f4
ReyNaldo1904 <114126394+ReyNaldo1904@users.noreply.github.com>
2023-10-20 17:02:55 -0600

    Semana 6 Productos

---
+++ src/main/java/com/tienda_v1/domain/Producto.java
@@ -0,0 +1,39 @@
+package com.tienda_v1.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table (name="producto")
+public class Producto implements Serializable{
+    
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_producto")
+    private Long idProducto;
+    
+    private String descripcion;
+    private String rutaImagen;
+    private Boolean activo;
+    
+
+public Producto(){
+
+}
+
+    public Producto(String descripcion, Boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+
+}
+    
+
+
+ 
+
--- src/main/java/com/tienda_v1/service/CategoriaService.java
+++ src/main/java/com/tienda_v1/service/CategoriaService.java
@@ -1,9 +1,22 @@
 package com.tienda_v1.service;
 
 import com.tienda_v1.domain.Categoria;
+import com.tienda_v1.domain.Producto;
 import java.util.List;
 
 
 public interface CategoriaService {
-    public List<Categoria> getCategorias(boolean activo);   
+    public List<Producto> getCategorias(boolean activo);  
+     // Se obtiene un Producto, a partir del id de un categoria
+    public Producto getCategoria(Producto categoria);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
+
+    public Categoria getCategoria(Categoria categoria);
 }
+
