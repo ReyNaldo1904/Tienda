5f04d8cabb440ff1f2bc6bbda14c44e882888808
ReyNaldo1904 <114126394+ReyNaldo1904@users.noreply.github.com>
2023-10-22 18:17:14 -0600

    Semana 6 Productos 2.0

--- src/main/java/com/tienda_v1/service/CategoriaService.java
+++ src/main/java/com/tienda_v1/service/CategoriaService.java
@@ -1,14 +1,14 @@
 package com.tienda_v1.service;
 
 import com.tienda_v1.domain.Categoria;
-import com.tienda_v1.domain.Producto;
+
 import java.util.List;
 
 
 public interface CategoriaService {
-    public List<Producto> getCategorias(boolean activo);  
+    public List<Categoria> getCategorias(boolean activo);  
      // Se obtiene un Producto, a partir del id de un categoria
-    public Producto getCategoria(Producto categoria);
+    public Categoria getCategoria(Categoria categoria);
     
     // Se inserta un nuevo categoria si el id del categoria esta vacío
     // Se actualiza un categoria si el id del categoria NO esta vacío
@@ -17,6 +17,6 @@ public interface CategoriaService {
     // Se elimina el categoria que tiene el id pasado por parámetro
     public void delete(Categoria categoria);
 
-    public Categoria getCategoria(Categoria categoria);
+
 }
 
