828450e9835f00e589f3c2dd9f27a3004db08c77
ReyNaldo1904 <114126394+ReyNaldo1904@users.noreply.github.com>
2023-11-10 14:02:45 -0600

    Semana 9 Pruebas Fragmentos

--- src/main/java/com/tienda_v1/serviceimpl/ProductoServiceImpl.java
+++ src/main/java/com/tienda_v1/serviceimpl/ProductoServiceImpl.java
@@ -7,20 +7,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class ProductoServiceImpl implements ProductoService {
+
     @Autowired
     private ProductoDao productoDao;
-    
+
     @Override
     public List<Producto> getProductos(boolean activo) {
-        var productos= productoDao.findAll();
+        var productos = productoDao.findAll();
         if (activo) {
             productos.removeIf(e -> !e.getActivo());
         }
         return productos;
     }
-     @Override
+
+    @Override
     @Transactional(readOnly = true)
     public Producto getProducto(Producto producto) {
         return productoDao.findById(producto.getIdProducto()).orElse(null);
@@ -37,6 +40,10 @@ public class ProductoServiceImpl implements ProductoService {
     public void delete(Producto producto) {
         productoDao.delete(producto);
     }
-   
+    //lista de productos utilizando metodo Query
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> consultaQuery(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
 }
-
